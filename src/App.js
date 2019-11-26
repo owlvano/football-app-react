ca67c2c849a55f03b63c6cde548e95cee2de60f5
Ivan Sova <owlvano@gmail.com>
2019-11-26 16:30:19 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { CompetitionsContainer } from './components/Competitions';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export const App = () => (
-    <div id="container"> 
-        <CompetitionsContainer />
-    </div>
+    <Router>
+        <div id="container"> 
+            <Switch>
+                <Route exact path="/" component={CompetitionsContainer}/>
+            </Switch>
+        </div>
+    </Router>
 );
