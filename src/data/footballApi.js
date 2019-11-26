03ff9659dc77d23777b9d134638c95edff447b9a
Ivan Sova <owlvano@gmail.com>
2019-11-26 14:15:33 +0200

    move headers

--- src/data/footballApi.js
+++ src/data/footballApi.js
@@ -1,7 +1,9 @@
 import axios  from 'axios';
-import { HEADERS } from './headers';
 
 const apiUrl = "http://api.football-data.org/v2/";
+const HEADERS = {
+    'X-Auth-Token': '039441a2aed54418a0a05234a1648399'
+};
 
 export const getCompetitions = () =>  
     axios.get(apiUrl + "competitions/", { headers: HEADERS })
--- src/data/headers.js
+++
@@ -1,3 +0,0 @@
-export const HEADERS = {
-    'X-Auth-Token': '039441a2aed54418a0a05234a1648399'
-};
