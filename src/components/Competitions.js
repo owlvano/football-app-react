46324a0f8666cf891e09a49f90c8d2c8c2175926
Ivan Sova <owlvano@gmail.com>
2019-11-26 04:47:01 +0200

    load data from api

--- src/components/Competitions.js
+++ src/components/Competitions.js
@@ -1,14 +1,59 @@
 import React from 'react';
-import jsonData from '../data/mockData.json';
-const loadData = () => JSON.parse(JSON.stringify(jsonData));
+import { ErrorComponent, handleError } from './Error';
+import { HEADERS } from '../data/headers';
+
 
 export class CompetitionsContainer extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            competitions: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://api.football-data.org/v2/competitions/", {
+            crossDomain: true,
+            method: 'GET',
+            headers: HEADERS
+        })
+        .then(res => res.json())
+        .then(handleError)
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    competitions: result.competitions
+                });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error: error
+                });
+            }
+        )
     }
 
     render() {
-        return <CompetitionsList competitions={loadData().competitions}/>;
+        const { error, isLoaded, competitions } = this.state;
+        if (error) {
+            return <ErrorComponent error={error} />;
+        } else if (!isLoaded) {
+            return <div id="content"><h1>Loading...</h1></div>;
+        } else {
+            return (
+                <div id="content">
+                    <h1>Competitions</h1>
+                    <CompetitionsList competitions={competitions}/>
+                </div>
+            );
+        }
     }
 }
 
@@ -17,12 +62,7 @@ const CompetitionsList = props => {
     const listItems = competitions.map(
         (competition, i) => <CompetitionRow key={i} competition={competition} />
     );
-    return (
-        <div id="content">
-            <h1>Competitions</h1>
-            <ul>{listItems}</ul>
-        </div>
-    );
+    return <ul>{listItems}</ul>;
 } 
 
 const CompetitionRow = props => {
