9d54459f9045f021a957b9b2800a3b7ac7452b51
Ivan Sova <owlvano@gmail.com>
2019-11-26 18:00:41 +0200

    utc date format

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Moment from 'react-moment';
 import { getMatchesData, WINNER } from '../data/footballApi';
 import { ErrorComponent, LoadingComponent } from './Helpers';
 
